7259ce80f078b180519027b47d2702aae174a453
Hian <hian.hsm@gmail.com>
2023-01-31 13:45:10 -0300

    fix: filtro de partidas

--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -5,18 +5,17 @@ export default class MatchService {
   constructor(private _matchModel = Match) {}
 
   public async getMatches(inProgress: string | undefined) {
-    let status;
-
-    if (inProgress) {
-      status = inProgress === 'true';
-    }
-
     const matches = await this._matchModel.findAll({
-      where: { inProgress: status },
       include: [
         { model: Team, as: 'homeTeam', attributes: ['teamName'] },
         { model: Team, as: 'awayTeam', attributes: ['teamName'] },
       ] });
+    if (inProgress === 'true') {
+      return matches.filter((m) => m.dataValues.inProgress === true);
+    }
+    if (inProgress === 'false') {
+      return matches.filter((m) => m.dataValues.inProgress === false);
+    }
     return matches;
   }
 }
