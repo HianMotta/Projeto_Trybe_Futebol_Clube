60baeceb6f69cfcadedf714de34708bc78a1de3e
Hian <hian.hsm@gmail.com>
2023-04-11 10:33:30 -0300

    chore: deleta ExampleModel

--- app/backend/src/tests/mocks/loginMock.ts
+++ app/backend/src/tests/mocks/loginMock.ts
@@ -10,7 +10,7 @@ const login = {
     password: 'secret_admin',
 }
 
-const validToken = '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2NzU4NjQzNDcsImV4cCI6MTY3NTk1MDc0N30.gbftRamknJANlPJwNNBi7hR-pgkx24alnEAJHzMADuM'
 
 export {
     user,
